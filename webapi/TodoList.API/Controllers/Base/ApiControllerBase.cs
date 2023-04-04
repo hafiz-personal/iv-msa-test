9fe81355a2bf8c4456cb7f0564c019cf668c4af0
Hafiz <mn.hafiz@cloud-connect.asia>
2023-04-04 22:39:58 +0800

    init

---
+++ webapi/TodoList.API/Controllers/Base/ApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.API.Controllers.Base
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+    }
+}
---
+++ webapi/TodoList.API/Controllers/TasksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoList.API.Controllers.Base;
+
+namespace TodoList.API.Controllers
+{   
+    public class TasksController : ApiControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("test");
+        }
+    }
+}
